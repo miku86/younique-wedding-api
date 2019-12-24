7cfdd7bf0524511f849f43be47b6a9f560d992a6
miku86 <miku86coding@gmail.com>
2019-12-24 16:14:26 +0100

    Add delete handler

---
+++ deleteTodo.js
@@ -0,0 +1,19 @@
+import * as databaseLib from "./libs/database";
+import { failure, success } from "./libs/response";
+
+export const main = async (event) => {
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      todoId: event.pathParameters.id
+    }
+  };
+
+  try {
+    await databaseLib.call("delete", params);
+    return success({ status: true });
+  } catch (error) {
+    return failure({ status: false });
+  }
+}
