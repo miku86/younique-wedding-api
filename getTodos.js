5dcff93dd96bf7cb97c25374262aef3229375c1d
miku86 <miku86coding@gmail.com>
2019-12-24 15:56:33 +0100

    Add handler to get todos

---
+++ getTodos.js
@@ -0,0 +1,19 @@
+import * as databaseLib from "./libs/database";
+import { failure, success } from "./libs/response";
+
+export const main = () => {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await databaseLib.call("query", params);
+    return success(result.Items);
+  } catch (error) {
+    return failure({ status: false });
+  }
+}
