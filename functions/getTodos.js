8f0ea2668b204aa334cb6a8da5cab80f67157829
miku86 <miku86coding@gmail.com>
2020-01-07 20:15:14 +0100

    Return correct stuff

--- functions/getTodos.js
+++ functions/getTodos.js
@@ -3,25 +3,19 @@ import { failure, success } from "../libs/response";
 
 export const main = async (event) => {
   const userId = databaseLib.findUserId(event);
-  const pk = `USER#${userId}`;
-  const sk = `TODO#USER#${userId}`;
 
   const params = {
     TableName: process.env.tableName,
     KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
     ExpressionAttributeValues: {
-      ":pk": pk,
-      ":sk": sk
+      ":pk": `USER#${userId}`,
+      ":sk": `TODO#USER#${userId}`
     }
   };
 
   try {
-    await databaseLib.call("query", params);
-    console.log(userId);
-    return success({
-      userId,
-    }
-    );
+    const result = await databaseLib.call("query", params);
+    return success(result.Items);
   } catch (error) {
     return failure({ status: false });
   }
