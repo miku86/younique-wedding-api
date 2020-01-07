2188829f071dcc34378a2a78ffb477ddbec63e94
miku86 <miku86coding@gmail.com>
2020-01-07 19:17:38 +0100

    Hardcode value

--- functions/getTodos.js
+++ functions/getTodos.js
@@ -2,13 +2,13 @@ import * as databaseLib from "../libs/database";
 import { failure, success } from "../libs/response";
 
 export const main = async (event) => {
-  const userId = event.requestContext.identity.cognitoIdentityId;
+  // const userId = event.requestContext.identity.cognitoIdentityId;
 
   const params = {
     TableName: process.env.tableName,
     KeyConditionExpression: "PK = :pk",
     ExpressionAttributeValues: {
-      ":pk": { S: `USER#${userId}` }
+      ":pk": "USER#792bdd6d-c98c-4b81-a9da-64b3a5de8478"
     }
   };
 
