2665bd1f19b76102841128c112ac6f8268959df8
miku86 <miku86coding@gmail.com>
2020-01-07 19:39:09 +0100

    Change from hardcoded to var

--- functions/getTodos.js
+++ functions/getTodos.js
@@ -2,14 +2,14 @@ import * as databaseLib from "../libs/database";
 import { failure, success } from "../libs/response";
 
 export const main = async (event) => {
-  // const userId = event.requestContext.identity.cognitoIdentityId;
+  const userId = event.requestContext.identity.cognitoIdentityId;
 
   const params = {
     TableName: process.env.tableName,
     KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
     ExpressionAttributeValues: {
-      ":pk": "USER#792bdd6d-c98c-4b81-a9da-64b3a5de8478",
-      ":sk": "TODO#USER#792bdd6d-c98c-4b81-a9da-64b3a5de8478"
+      ":pk": `USER#${userId}`,
+      ":sk": `TODO#USER#792bdd6d-c98c-4b81-a9da-64b3a5de8478`
     }
   };
 
