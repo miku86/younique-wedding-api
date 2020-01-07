47b6b7bc8263e46162a2dee97ab2c93c26128521
miku86 <miku86coding@gmail.com>
2020-01-07 18:59:27 +0100

    Add sk

--- functions/getTodos.js
+++ functions/getTodos.js
@@ -2,15 +2,14 @@ import * as databaseLib from "../libs/database";
 import { success } from "../libs/response";
 
 export const main = async (event) => {
-  console.log(event);
   const userId = event.requestContext.identity.cognitoIdentityId;
-  console.log(userId);
 
   const params = {
     TableName: process.env.tableName,
-    KeyConditionExpression: "PK = :pk",
+    KeyConditionExpression: "PK = :pk AND BEGINS_WITH (SK, :sk)",
     ExpressionAttributeValues: {
-      ":pk": `USER#${userId}`
+      ":pk": `USER#${userId}`,
+      ":sk": `TODO#USER#${userId}`
     }
   };
 
@@ -19,7 +18,6 @@ export const main = async (event) => {
     console.log(result);
     return success(result.Items);
   } catch (error) {
-    // return failure({ status: false });
-    return error;
+    return failure({ status: false });
   }
 };
