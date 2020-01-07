b3ab77f2987b5be4914945d8332a9f8ca224d87d
miku86 <miku86coding@gmail.com>
2020-01-07 16:30:01 +0100

    Return correct stuff

--- libs/database.js
+++ libs/database.js
@@ -4,3 +4,10 @@ export const call = (action, params) => {
   const database = new AWS.DynamoDB.DocumentClient();
   return database[action](params).promise();
 };
+
+export const findUserId = (event) => {
+  const authProvider = event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(':');
+  const userPoolUserId = parts[parts.length - 1];
+  return userPoolUserId;
+};
