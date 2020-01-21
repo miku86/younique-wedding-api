6d541febd8418ed97beeb41b550c81739a29da3a
miku86 <miku86coding@gmail.com>
2020-01-21 12:12:47 +0100

    Add function to fetch dashboard data

---
+++ functions/getDashboard.js
@@ -0,0 +1,54 @@
+import * as databaseLib from "../libs/database";
+import { failure } from "../libs/response";
+
+const fetchTodoData = async (userId) => {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": `USER#${userId}`,
+      ":sk": `TODO#${userId}`
+    }
+  };
+
+  try {
+    const { Items } = await databaseLib.call("query", params);
+    const amountItems = Items.length;
+    const amountDoneItems = Items.filter(item => item.done).length;
+    return { amountItems, amountDoneItems };
+  } catch (error) {
+    return failure({ status: false });
+  }
+};
+
+const fetchGuestData = async (userId) => {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": `USER#${userId}`,
+      ":sk": `GUEST#${userId}`
+    }
+  };
+
+  try {
+    const { Items } = await databaseLib.call("query", params);
+    const amountItems = Items.length;
+    const amountDoneItems = Items.filter(item => item.coming).length;
+    return { amountItems, amountDoneItems };
+  } catch (error) {
+    return failure({ status: false });
+  }
+};
+
+export const main = async (event) => {
+  const userId = databaseLib.findUserId(event);
+
+  let dashboardData = {};
+  dashboardData.todos = fetchTodoData(userId);
+  dashboardData.guests = fetchGuestData(userId);
+
+  console.log(dashboardData);
+
+  return dashboardData;
+};
