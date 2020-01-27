4a22c1f0bfbc1b033583626adfb92ac058b5c797
miku86 <miku86coding@gmail.com>
2020-01-27 16:23:36 +0100

    Remove duplicate code

--- libs/database.js
+++ libs/database.js
@@ -11,3 +11,33 @@ export const findUserId = (event) => {
   const userPoolUserId = parts[parts.length - 1];
   return userPoolUserId;
 };
+
+export const createNames = (data) => {
+  const names = {};
+  Object.keys(data).forEach(key => {
+    names[`#${key}`] = key;
+  });
+  return names;
+};
+
+export const createValues = (data) => {
+  const values = {};
+  Object.keys(data).forEach(key => {
+    if (typeof data[key] === "boolean") {
+      values[`:${key}`] = data[key];
+    } else if (data[key] === "" || data[key] === null) {
+      values[`:${key}`] = null;
+    } else {
+      values[`:${key}`] = `${data[key]}`;
+    }
+  });
+  return values;
+};
+
+export const createExpression = (data) => {
+  const expression = Object
+    .keys(data)
+    .map(key => `#${[key]} = :${key}`)
+    .join(", ");
+  return `SET ${expression}`;
+};
