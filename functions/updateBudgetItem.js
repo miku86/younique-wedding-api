7ad011c9ec1bac2853c1c89802373dd6f16da217
miku86 <miku86coding@gmail.com>
2020-01-27 15:27:50 +0100

    Add function

--- functions/updateBudgetItem.js
+++ functions/updateBudgetItem.js
@@ -1,9 +1,43 @@
 import * as databaseLib from "../libs/database";
 import { failure, success } from "../libs/response";
 
+const createNames = (data) => {
+  const names = {};
+  Object.keys(data).forEach(key => {
+    names[`#${key}`] = key;
+  });
+  return names;
+};
+
+const createValues = (data) => {
+  const values = {};
+  Object.keys(data).forEach(key => {
+    if (typeof data[key] === "boolean") {
+      values[`:${key}`] = data[key];
+    } else if (data[key] === "" || data[key] === null) {
+      values[`:${key}`] = null;
+    } else {
+      values[`:${key}`] = `${data[key]}`;
+    }
+  });
+  return values;
+};
+
+const createExpression = (data) => {
+  const expression = Object
+    .keys(data)
+    .map(key => `#${[key]} = :${key}`)
+    .join(", ");
+  return `SET ${expression}`;
+};
+
 export const main = async (event) => {
-  const { budgetItemId, fieldKey, newFieldValue } = JSON.parse(event.body);
+  const { budgetItemId, data } = JSON.parse(event.body);
   const userId = databaseLib.findUserId(event);
+
+  const UpdateExpression = createExpression(data);
+  const ExpressionAttributeNames = createNames(data);
+  const ExpressionAttributeValues = createValues(data);
 
   const params = {
     TableName: process.env.tableName,
@@ -11,10 +45,9 @@ export const main = async (event) => {
       PK: `USER#${userId}`,
       SK: `BUDGETITEM#${userId}#${budgetItemId}`,
     },
-    UpdateExpression: `SET ${fieldKey} = :newFieldValue`,
-    ExpressionAttributeValues: {
-      ":newFieldValue": newFieldValue
-    },
+    UpdateExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
     ReturnValues: "ALL_NEW"
   };
 
